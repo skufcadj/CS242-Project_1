8968f9324e68480f94ce422709fc59f5a2fedfff
skufcadj <skufcadj@clarkson.edu>
2016-11-19 21:48:37 -0500

    Add files via upload

---
+++ dragMenu/src/application/ListOrganizer.java
@@ -0,0 +1,160 @@
+package application;
+import javafx.application.Application;
+import javafx.collections.*;
+import javafx.geometry.*;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
+import javafx.scene.input.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import java.util.*;
+
+public class ListOrganizer extends Application {
+    private static final String PREFIX =
+            "https://raw.githubusercontent.com/skufcadj/CS242-Project_1/dave/shapes/";
+
+    private static final String SUFFIX =
+            ".png";
+
+    private static final ObservableList<String> birds = FXCollections.observableArrayList(
+            "red_circle",
+            "red_square",
+            "blue_square",
+            "yellow_triangle",
+            "green_triangle",
+            "green_square",
+            "blue_diamond"
+    );
+
+    private static final ObservableList<Image> birdImages = FXCollections.observableArrayList();
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        birds.forEach(bird -> birdImages.add(new Image( bird + SUFFIX)));
+
+        ListView<String> birdList = new ListView<>(birds);
+        birdList.setCellFactory(param -> new BirdCell());
+        birdList.setPrefWidth(180);
+
+        VBox layout = new VBox(birdList);
+        layout.setPadding(new Insets(10));
+
+        stage.setScene(new Scene(layout));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(ListOrganizer.class);
+    }
+
+    private class BirdCell extends ListCell<String> {
+        private final ImageView imageView = new ImageView();
+
+        public BirdCell() {
+            ListCell thisCell = this;
+
+            setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+            setAlignment(Pos.CENTER);
+
+            setOnDragDetected(event -> {
+                if (getItem() == null) {
+                    return;
+                }
+
+                ObservableList<String> items = getListView().getItems();
+
+                Dragboard dragboard = startDragAndDrop(TransferMode.MOVE);
+                ClipboardContent content = new ClipboardContent();
+                content.putString(getItem());
+                dragboard.setDragView(
+                    birdImages.get(
+                        items.indexOf(
+                            getItem()
+                        )
+                    )
+                );
+                dragboard.setContent(content);
+
+                event.consume();
+            });
+
+            setOnDragOver(event -> {
+                if (event.getGestureSource() != thisCell &&
+                       event.getDragboard().hasString()) {
+                    event.acceptTransferModes(TransferMode.MOVE);
+                }
+
+                event.consume();
+            });
+
+            setOnDragEntered(event -> {
+                if (event.getGestureSource() != thisCell &&
+                        event.getDragboard().hasString()) {
+                    setOpacity(0.3);
+                }
+            });
+
+            setOnDragExited(event -> {
+                if (event.getGestureSource() != thisCell &&
+                        event.getDragboard().hasString()) {
+                    setOpacity(1);
+                }
+            });
+
+            setOnDragDropped(event -> {
+                if (getItem() == null) {
+                    return;
+                }
+
+                Dragboard db = event.getDragboard();
+                boolean success = false;
+
+                if (db.hasString()) {
+                    ObservableList<String> items = getListView().getItems();
+                    int draggedIdx = items.indexOf(db.getString());
+                    int thisIdx = items.indexOf(getItem());
+
+                    Image temp = birdImages.get(draggedIdx);
+                    birdImages.set(draggedIdx, birdImages.get(thisIdx));
+                    birdImages.set(thisIdx, temp);
+
+                    items.set(draggedIdx, getItem());
+                    items.set(thisIdx, db.getString());
+
+                    List<String> itemscopy = new ArrayList<>(getListView().getItems());
+                    getListView().getItems().setAll(itemscopy);
+
+                    success = true;
+                }
+                event.setDropCompleted(success);
+
+                event.consume();
+            });
+
+            setOnDragDone(DragEvent::consume);
+        }
+
+        @Override
+        protected void updateItem(String item, boolean empty) {
+            super.updateItem(item, empty);
+
+            if (empty || item == null) {
+                setGraphic(null);
+            } else {
+                imageView.setImage(
+                    birdImages.get(
+                        getListView().getItems().indexOf(item)
+                    )
+                );
+                setGraphic(imageView);
+            }
+        }
+    }
+
+    // Iconset Homepage: http://jozef89.deviantart.com/art/Origami-Birds-400642253
+    // License: CC Attribution-Noncommercial-No Derivate 3.0
+    // Commercial usage: Not allowed    
+
+}
